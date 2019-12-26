b66f3c8f40be25474669abe6b894daad63f42b2a
Felipe Santos <felipecassiors@gmail.com>
2019-12-26 00:37:32 -0300

    Add tab indentation for sh files
    This way, <<-EOF works.

--- ci/test_deployed_box.sh
+++ ci/test_deployed_box.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 (
-  echo "Testing the deployed box..." && \
-  set -x && \
-  echo "  Fetching download url..." && \
-  readonly DOWNLOAD_URL=$(curl -fsS \
-    --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-    "$PROVIDER_BASE_URL" \
-    | jq -r .download_url) && \
+	echo "Testing the deployed box..." && \
+	set -x && \
+	echo "  Fetching download url..." && \
+	readonly DOWNLOAD_URL=$(curl -fsS \
+		--header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+		"$PROVIDER_BASE_URL" \
+		| jq -r .download_url) && \
 
-  echo "  Adding box to vagrant..." && \
-  readonly TEST_BOX="test-deploy" && \
-  vagrant box add "$TEST_BOX" "$DOWNLOAD_URL" && \
+	echo "  Adding box to vagrant..." && \
+	readonly TEST_BOX="test-deploy" && \
+	vagrant box add "$TEST_BOX" "$DOWNLOAD_URL" && \
 
-  echo "  Performing vagrant init and up..." && \
-  readonly TEST_FOLDER="temp/" && \
-  ( rmdir "$TEST_FOLDER" &> /dev/null; mkdir "$TEST_FOLDER" ) && \
-  pushd "$TEST_FOLDER" > /dev/null && \
-  vagrant init "$TEST_BOX" && \
-  vagrant up && \
-  popd > /dev/null
+	echo "  Performing vagrant init and up..." && \
+	readonly TEST_FOLDER="temp/" && \
+	( rmdir "$TEST_FOLDER" &> /dev/null; mkdir "$TEST_FOLDER" ) && \
+	pushd "$TEST_FOLDER" > /dev/null && \
+	vagrant init "$TEST_BOX" && \
+	vagrant up && \
+	popd > /dev/null
 )
