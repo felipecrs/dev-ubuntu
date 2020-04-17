ee58ad7743ed7f39ee33daea85b23e53f3c7c4e2
Felipe Santos <felipecassiors@gmail.com>
2020-04-16 22:59:51 -0300

    ci: create deploy script for test branch

---
+++ ci/deploy_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+box='felipecassiors/ubuntu1804-4dev-test'
+version="$(date +'%Y%m.%d.%H%M')"
+provider='virtualbox'
+file='package.box'
+description="[**View source code in GitHub**](https://github.com/felipecassiors/ubuntu1804-4dev/commit/$(git rev-parse HEAD))"
+short_description='Testing releases from https://github.com/felipecassiors/ubuntu1804-4dev'
+checksum="$(md5sum "$file" | cut -d " " -f 1)"
+
+vagrant cloud auth login
+vagrant cloud publish --force \
+	--checksum-type md5 \
+	--checksum "$checksum" \
+	--description "$description" \
+	--short-description "$short_description" \
+	"$box" "$version" "$provider" "$file" ||
+	{
+		echo "Ignoring fail, this is probably a server issue."
+		echo "Sleeping some time for the box to be available on cloud..."
+		sleep 5m
+	}
+export PROVIDER_BASE_URL="https://app.vagrantup.com/api/v1/box/$box/version/$version/provider/$provider"
+"${__dir}/test_deployed_box.sh"
+yes | vagrant cloud version release "$box" "$version"
