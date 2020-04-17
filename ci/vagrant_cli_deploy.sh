93defbc741cb13dd1fcdb89d601670f11b9735a7
Felipe Santos <felipecassiors@gmail.com>
2020-04-16 22:59:51 -0300

    ci: create deploy script for test branch

--- ci/vagrant_cli_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-vagrant cloud auth login
-vagrant cloud publish --force "$box" "$version" "$provider" "$file"
-${__dir}/test_deployed_box.sh
-vagrant cloud release "$box" "$version"
