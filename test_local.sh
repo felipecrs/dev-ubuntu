b2e38afacf3bcc05aa8707e7baf5abdef5a9fe8e
Felipe Santos <felipecassiors@gmail.com>
2020-05-20 20:30:58 -0300

    test: add script for testing locally

---
+++ test_local.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+vagrant destroy -f
+vagrant box update
+vagrant up
+vagrant package --vagrantfile src/Vagrantfile
+vagrant box add test package.box
+rm -f package.box
+mkdir test || true
+pushd test
+vagrant init test
+vagrant up
+echo; read -rsn1 -p "Press any key to continue . . ."; echo
+vagrant destroy -f
+vagrant box remove test
+popd
+rm -rf test
