cd092c794b962441ff4a5a7b12e71ec7c40cee20
Felipe Santos <felipecassiors@gmail.com>
2020-05-22 16:22:05 -0300

    adjust action

--- tools/manual_release.sh
+++ tools/manual_release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 curl -X POST https://api.github.com/repos/felipecassiors/dev-ubuntu-20.04/dispatches \
-	-H 'Authorization: token $TOKEN' \
+	-H "Authorization: token $TOKEN" \
 	--data '{"event_type": "manual_trigger"}'
